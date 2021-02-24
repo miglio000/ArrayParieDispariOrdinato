0eaaface5f2b3aa6157f055101af120f97689b70
miglio000 <gabrielemiglio23@gmail.com>
2021-02-24 13:23:04 +0100

    Add project files.

---
+++ ArrayOrdinato_piuArrayParieDispari/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using LibreriaArray;
+namespace ArrayOrdinato_piuArrayParieDispari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c_pari = 0, c_disp = 0;
+            Console.WriteLine("Inserisci dimensione array: ");
+            int x = int.Parse(Console.ReadLine());
+            int[] array_random = new int[x];
+            Gestione.RiempimentoRandom(array_random);
+            Gestione.BubbleSort(array_random, x);
+            for (int i = 0; i < array_random.Length; i++)
+            {
+                if (array_random[i] % 2 == 0)
+                    c_pari++;
+                else
+                    c_disp++;
+            }
+            int[] array_pari = new int[c_pari];
+            int[] array_dispari = new int[c_disp];
+            for (int i = 0, j = 0, k = 0; i < x; i++)
+            {
+                if (array_random[i] % 2 == 0)
+                {
+                    array_pari[j] = array_random[i];
+                    j++;
+                }
+                else
+                {
+                    array_dispari[k] = array_random[i];
+                    k++;
+                }
+            }
+            Console.WriteLine("Array pari: ");
+            Gestione.StampaArray(array_pari);
+            Console.WriteLine("Array dispari: ");
+            Gestione.StampaArray(array_dispari);
+            Console.WriteLine("Array ordinato: ");
+            Gestione.StampaArray(array_random);
+        }
+    }
+}
---
+++ LibreriaArray/Gestione.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace LibreriaArray
+{
+    public class Gestione
+    {
+        public static void RiempimentoRandom(int[] array)
+        {
+            Random r = new Random();
+            for(int i = 0; i < array.Length; i++)
+            {
+                array[i] = r.Next(101);
+            }
+        }
+        public static void BubbleSort(int[] n, int tanti)
+        {
+            int x, y;
+            int temp;
+            for (x = 0; x < tanti - 1; x++)
+            {
+                for (y = 0; y < tanti - 1 - x; y++)
+                {
+                    if (n[y] > n[y + 1])
+                    {
+                        temp = n[y];
+                        n[y] = n[y + 1];
+                        n[y + 1] = temp;
+                    }
+                }
+            }
+        }
+        public static void StampaArray(int[] array)
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
